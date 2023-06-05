68135811f81b5d03d3abe205c591bd95b35e334e
z <42330405+Sfree973@users.noreply.github.com>
2023-06-05 12:52:12 +1000

    Update Sampling.py

--- rl/graph_search/Sampling.py
+++ rl/graph_search/Sampling.py
@@ -5,77 +5,98 @@ import numpy as np
 import sys
 import time
 import math
-from Bi-BFS.Bi-BFS import BFS
-from Bi-BFS.KB import KB
 from itertools import count
 from networks import discriminator_nn
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 
+# Import functions from rule_learning.py and rule_application.py
+from rule_learning import learn_rules
+from rule_application import apply_rules, select_high_score_paths
 
-LAMBDA = 5  # Gradient penalty lambda hyper-parameter.
+class KGDataset(Dataset):
+    def __init__(self, path):
+        f = open(path)
+        content = f.readlines()
+        f.close()
+        kb = KB()
+        for line in content:
+            ent1, rel, ent2 = line.rsplit()
+            kb.addRelation(ent1, rel, ent2)
+        self.kb = kb
 
-class Discriminator(nn.Module):
-    def __init__(self, batch_size, embedding_dim, learning_rate=0.001):
-        super(Discriminator, self).__init__()
+        f = open(relationPath)
+        train_data = f.readlines()
+        f.close()
 
-        self.initializer = nn.init.xavier_uniform_
-        self.task = 'train'  # Placeholder for task (string)
+        self.samples = []
+        for sample in train_data:
+            ent1, rel, ent2 = sample.split()
+            self.samples.append((ent1, rel, ent2))
 
-        self.real_inputs = nn.Parameter(torch.Tensor(batch_size, 1, embedding_dim))
-        self.fake_inputs = nn.Parameter(torch.Tensor(batch_size, 1, embedding_dim))
+    def __len__(self):
+        return len(self.samples)
 
-        self.disc_nn_real = discriminator_nn(embedding_dim, self.initializer)
-        self.disc_nn_fake = discriminator_nn(embedding_dim, self.initializer)
+    def __getitem__(self, idx):
+        ent1, rel, ent2 = self.samples[idx]
+        return ent1, rel, ent2
 
-        self.optimizer = optim.SGD(self.parameters(), lr=learning_rate)
+class RuleNet(nn.Module):
+    def __init__(self, num_entities, num_relations, rule_lengths):
+        super(RuleNet, self).__init__()
+        self.rule_lengths = rule_lengths
 
-    def forward(self, x):
-        return self.disc_nn_real(x)
+        self.entity_embeddings = nn.Embedding(num_entities, embedding_dim)
+        self.relation_embeddings = nn.Embedding(num_relations, embedding_dim)
 
-    def predict(self, real, fake):
-        self.eval()
-        with torch.no_grad():
-            disc_real = self.forward(real)
-            disc_fake = self.forward(fake)
-            original_disc_cost = torch.mean(disc_fake) - torch.mean(disc_real)
+        self.rule_scores = nn.ModuleDict()
+        for rule_length in rule_lengths:
+            self.rule_scores[str(rule_length)] = nn.Linear(embedding_dim * rule_length, 1)
 
-            # WGAN Lipschitz penalty
-            alpha = torch.rand(real.size(0), 1, 1)
-            if torch.cuda.is_available():
-                alpha = alpha.cuda()
-            interpolate = alpha * self.real_inputs + (1 - alpha) * self.fake_inputs
-            interpolate.requires_grad_(True)
+    def forward(self, ent1, rel, ent2, rule_walks):
+        ent1_emb = self.entity_embeddings(ent1)
+        rel_emb = self.relation_embeddings(rel)
+        ent2_emb = self.entity_embeddings(ent2)
 
-            disc_interpolate = self.forward(interpolate)
-            gradients = torch.autograd.grad(outputs=disc_interpolate, inputs=interpolate,
-                                            grad_outputs=torch.ones(disc_interpolate.size()),
-                                            create_graph=True, retain_graph=True)[0]
+        rule_scores = []
+        for rule_length in self.rule_lengths:
+            rule_score = self.rule_scores[str(rule_length)](rule_walks[:, :rule_length].reshape(-1, embedding_dim * rule_length))
+            rule_scores.append(rule_score)
+        rule_scores = torch.cat(rule_scores, dim=1)
 
-            slopes = torch.sqrt(torch.sum(gradients ** 2, dim=[1, 2]))
-            gradient_penalty = torch.mean((slopes - 1.) ** 2)
+        scores = torch.sum(ent1_emb * rel_emb * ent2_emb, dim=1) + rule_scores.squeeze(1)
+        return scores
 
-            disc_cost = original_disc_cost + LAMBDA * gradient_penalty
-            gen_reward = torch.mean(disc_fake)
+def sampling(path_threshold=2, path=None):
+    f = open(path)
+    content = f.readlines()
+    f.close()
+    kb = KB()
+    for line in content:
+        ent1, rel, ent2 = line.rsplit()
+        kb.addRelation(ent1, rel, ent2)
 
-            return disc_cost, gen_reward
+    f = open(relationPath)
+    train_data = f.readlines()
+    f.close()
 
-    def update(self, real, fake):
-        self.train()
-        self.optimizer.zero_grad()
-        disc_cost, _ = self.predict(real, fake)
-        disc_cost.backward()
-        self.optimizer.step()
-        return disc_cost.item()
+    num_samples = len(train_data)
+    demo_path_dict = {}
+    for episode in range(num_samples):
+        sample = train_data[episode % num_samples].split()
+        ent1 = sample[0]
+        ent2 = sample[2]
+        rel = sample[1]
 
-def discriminator_nn(embedding_dim, initializer):
-    # Define your discriminator neural network architecture here
-    # Note: You may need to modify the architecture to match your specific requirements
-    # For simplicity, I'm assuming a simple linear layer followed by a sigmoid activation
-    model = nn.Sequential(
-        nn.Linear(embedding_dim, 1),
-        nn.Sigmoid()
-    )
-    model.apply(initializer)
-    return model
+        kb.removePath(ent1, ent2)
+        try:
+            suc, entity_list, path_list = BFS(kb, ent1, ent2)
+            path_str = ' -> '.join(path_list)
+        except Exception as e:
+            print('Episode %d' % episode)
+            print('Cannot find a path')
+            continue
+
+        if path_str not in demo_path
